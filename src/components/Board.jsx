668d8ef6f769bfef476ef01ea50e0375b4ae5d40
Reena Soni <reenaprahladsoni@gmail.com>
2022-09-06 16:36:11 +0530

    swipe left functionality implemented

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import { Container } from "@material-ui/core";
+import { Button, Container } from "@material-ui/core";
 import Cell from "./Cell";
 import { cloneDeep } from "lodash";
 
 const Board = () => {
+  const LEFT_ARROW = 37;
   const [state, setState] = React.useState([
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -14,8 +15,8 @@ const Board = () => {
 
   const initialize = () => {
     const newGrid = cloneDeep(state);
-    addInitialNumbers(newGrid);
-    addInitialNumbers(newGrid);
+    addNumber(newGrid);
+    addNumber(newGrid);
     setState(newGrid);
   };
 
@@ -24,12 +25,68 @@ const Board = () => {
     // eslint-disable-next-line
   }, []);
 
-  const addInitialNumbers = (newGrid) => {
+  const addNumber = (newGrid) => {
     let rand1 = Math.floor(Math.random() * 4);
     let rand2 = Math.floor(Math.random() * 4);
 
     if (newGrid[rand1][rand2] === 0) {
       newGrid[rand1][rand2] = Math.random() > 0.5 ? 2 : 4;
+    }
+  };
+
+  function swipeLeft() {
+    let oldGrid = state;
+    let newArray = cloneDeep(state);
+
+    for (let i = 0; i < 4; i++) {
+      let b = newArray[i];
+      let slow = 0;
+      let fast = 1;
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1;
+          slow++;
+          continue;
+        }
+        if (b[slow] === 0 && b[fast] === 0) {
+          fast++;
+        } else if (b[slow] === 0 && b[fast] !== 0) {
+          b[slow] = b[fast];
+          b[fast] = 0;
+          fast++;
+        } else if (b[slow] !== 0 && b[fast] === 0) {
+          fast++;
+        } else if (b[slow] !== 0 && b[fast] !== 0) {
+          if (b[slow] === b[fast]) {
+            b[slow] = b[slow] + b[fast];
+            b[fast] = 0;
+            fast = slow + 1;
+            slow++;
+          } else {
+            slow++;
+            fast = slow + 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldGrid) !== JSON.stringify(newArray)) {
+      addNumber(newArray);
+    }
+    setState(newArray);
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("keydown", handleKeyEvents);
+  });
+
+  const handleKeyEvents = (event) => {
+    switch (event.keyCode) {
+      case LEFT_ARROW:
+        swipeLeft();
+        break;
+
+      default:
+        break;
     }
   };
 

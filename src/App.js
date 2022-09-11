45943d325f729146c9cb1bbacf3c125c7b56b704
Reena Soni <reenaprahladsoni@gmail.com>
2022-09-11 10:20:51 +0530

    commiting changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Board from "./components/Board";
+import Game from "./components/Game";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Board />
+    <div
+    // style={{
+    //   background: "linear-gradient(to right, #ad5389, #3c1053)",
+    // }}
+    >
+      {/* <Header />
+      <Board /> */}
+      <Game />
     </div>
   );
 }

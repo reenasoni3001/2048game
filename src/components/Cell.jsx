0df08952bb73dd3a6b8fec446309e4eacae5b8e5
Reena Soni <reenaprahladsoni@gmail.com>
2022-09-19 20:46:47 +0530

    added animation for cell

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,12 +1,31 @@
-import { Card, CardContent } from "@mui/material";
+import { Card, CardContent, keyframes, styled } from "@mui/material";
 import { getColors } from "../Util/getCellColors";
+
+const scaleUpCenter = keyframes`
+0% {
+  -webkit-transform: scale(0.5);
+          transform: scale(0.5);
+}
+100% {
+  -webkit-transform: scale(1);
+          transform: scale(1);
+}
+`;
+
+const Cells = styled(Card)(() => ({
+  height: "98px",
+  width: "98px",
+  "&:hover": {
+    animation: `${scaleUpCenter} 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both `,
+  },
+}));
 
 const Cell = ({ cell }) => {
   return (
-    <Card
+    <Cells
       sx={{
-        height: "98px",
-        width: "98px",
+        // height: "98px",
+        // width: "98px",
         bgcolor: cell === 0 ? "#FFF7CC" : getColors(cell),
       }}
     >
@@ -21,7 +40,7 @@ const Cell = ({ cell }) => {
           <span style={{ fontWeight: "bold", fontSize: 50 }}>{cell}</span>
         ) : null}
       </CardContent>
-    </Card>
+    </Cells>
   );
 };
 

009dffeef64741503dc526f717e360ca66be4345
Reena Soni <reenaprahladsoni@gmail.com>
2022-09-19 15:11:04 +0530

    added animation for title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,36 @@
-import { Box, Button, styled, Typography } from "@mui/material";
+import { Box, Button, keyframes, styled, Typography } from "@mui/material";
 import React from "react";
 import logo from "../assets/200w.webp";
 import { addNumber } from "../Util/handleSwipeFunctions";
 
+const textPopUpTop = keyframes`
+0% {
+  -webkit-transform: translateY(0);
+          transform: translateY(0);
+  -webkit-transform-origin: 50% 50%;
+          transform-origin: 50% 50%;
+  text-shadow: none;
+}
+100% {
+  -webkit-transform: translateY(-50px);
+          transform: translateY(-50px);
+  -webkit-transform-origin: 50% 50%;
+          transform-origin: 50% 50%;
+  text-shadow: 0 1px 0 #cccccc, 0 2px 0 #cccccc, 0 3px 0 #cccccc, 0 4px 0 #cccccc, 0 5px 0 #cccccc, 0 6px 0 #cccccc, 0 7px 0 #cccccc, 0 8px 0 #cccccc, 0 9px 0 #cccccc, 0 50px 30px rgba(0, 0, 0, 0.3);
+}
+
+`;
+
 const Title = styled(Typography)(() => ({
-  color: "#eee",
+  color: "#fff",
+  fontWeight: "bold",
+  "&:hover": {
+    animation: `${textPopUpTop} 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000) both `,
+  },
+}));
+
+const Description = styled(Typography)(() => ({
+  color: "#fff",
 }));
 
 const Header = ({ score, state, setState, setScore }) => {
@@ -31,11 +57,11 @@ const Header = ({ score, state, setState, setScore }) => {
         <Box component="img" sx={{ height: 90 }} alt="2048!" src={logo} />
       </Box>
       <Box display="flex" justifyContent="flex-start" flexDirection="column">
-        <Typography variant="h5">Join the tiles, get to 2048!</Typography>
-        <Typography variant="h6" component="p">
+        <Description variant="h5">Join the tiles, get to 2048!</Description>
+        <Description variant="h6" component="p">
           HOW TO PLAY: Use your arrow keys to move the tiles. Tiles with the
           same number merge into one when they touch. Add them up to reach 2048!
-        </Typography>
+        </Description>
       </Box>
       <Box
         display="flex"
@@ -55,8 +81,25 @@ const Header = ({ score, state, setState, setScore }) => {
             background: "#F6CD9B",
           }}
         >
-          <Typography>SCORE</Typography>
-          <Typography sx={{ p: 1 }}>{score}</Typography>
+          <Typography
+            sx={{
+              fontWeight: "bold",
+              color: "#281e1e",
+              fontSize: "20px",
+            }}
+          >
+            SCORE
+          </Typography>
+          <Typography
+            sx={{
+              p: 1,
+              fontWeight: "bold",
+              color: "#824646",
+              fontSize: "20px",
+            }}
+          >
+            {score}
+          </Typography>
         </Box>
         <Button
           p={2}
